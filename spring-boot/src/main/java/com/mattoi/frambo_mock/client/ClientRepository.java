5fb2081c887e89970434bcccde8a607b57de5bdf
mattoi <matheus.soares.8d@gmail.com>
2024-10-03 17:47:40 -0300

    Implemented JDBC methods

--- spring-boot/src/main/java/com/mattoi/frambo_mock/client/ClientRepository.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/client/ClientRepository.java
@@ -1,18 +1,51 @@
 package com.mattoi.frambo_mock.client;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
 @Repository
 public class ClientRepository {
 
-    private final JdbcClient jdbcClient;    
+    private final JdbcClient jdbcClient;
     private static final Logger log = LoggerFactory.getLogger(ClientRepository.class);
 
     public ClientRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
     }
 
-    //TODO post create(client fields..)
-    //TODO put update(client fields..)
-    //TODO get findAll()
-    //TODO get findById(id)
-    //TODO delete delete(id)
+    public void create(Client client) {
+        var updated = jdbcClient.sql("INSERT INTO Clients(client_name, email_address, phone_number) values(?,?,?)")
+                .params(List.of(client.name(), client.email(), client.phoneNumber())).update();
+        Assert.state(updated == 1, "Failed to create client " + client.name());
+    }
+
+    public void update(Client updatedClient) {
+        var updated = jdbcClient.sql(
+                "UPDATE Clients SET client_id = ?, client_name = ?, email_address = ?, phone_number = ? WHERE client_id = ?")
+                .params(List.of(updatedClient.id(), updatedClient.name(), updatedClient.email(),
+                        updatedClient.phoneNumber(), updatedClient.id()))
+                .update();
+        Assert.state(updated == 1, "Failed to update client " + updatedClient.name());
+    }
+
+    public List<Client> findAll() {
+        return jdbcClient.sql("SELECT * from Clients").query(Client.class).list();
+    }
+
+    public Client findById(Integer id) {
+        return jdbcClient.sql("SELECT * from Clients WHERE client_id = ")
+                .param("id", id).query(Client.class).single();
+    }
+
+    public void delete(Integer id) {
+        var updated = jdbcClient.sql("DELETE FROM Clients WHERE client_id = :id")
+                .param("id", id).update();
+
+        Assert.state(updated == 1, "Failed to delete client");
+    }
 }
