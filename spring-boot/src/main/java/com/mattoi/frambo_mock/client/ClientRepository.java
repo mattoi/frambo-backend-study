1af2f522a80eae2a0b2f068661e22dc76d8fa9cf
mattoi <matheus.soares.8d@gmail.com>
2024-10-09 01:35:04 -0300

    Fixed SQL grammar and array format

--- spring-boot/src/main/java/com/mattoi/frambo_mock/client/ClientRepository.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/client/ClientRepository.java
@@ -11,63 +11,64 @@ import org.springframework.util.Assert;
 @Repository
 public class ClientRepository {
 
-    private final JdbcClient jdbcClient;
-    private static final Logger log = LoggerFactory.getLogger(ClientRepository.class);
+        // TODO modify void methods to return booleans depending on success
+        private final JdbcClient jdbcClient;
+        private static final Logger log = LoggerFactory.getLogger(ClientRepository.class);
 
-    public ClientRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+        public ClientRepository(JdbcClient jdbcClient) {
+                this.jdbcClient = jdbcClient;
+        }
 
-    public void create(Client client) {
-        var updated = jdbcClient
-                .sql("INSERT INTO Clients(client_name, email_address, phone_number) values(?,?,?)")
-                .params(List.of(client.name(), client.email(), client.phoneNumber())).update();
-        Assert.state(updated == 1, "Failed to create client " + client.name());
-    }
+        public void create(Client client) {
+                var updated = jdbcClient
+                                .sql("INSERT INTO Clients(client_name, email_address, phone_number) values(?,?,?)")
+                                .params(List.of(client.name(), client.email(), client.phoneNumber())).update();
+                Assert.state(updated == 1, "Failed to create client " + client.name());
+        }
 
-    public void update(Client updatedClient) {
-        var updated = jdbcClient.sql(
-                "UPDATE Clients SET client_id = ?, client_name = ?, email_address = ?, phone_number = ? WHERE client_id = ?")
-                .params(List.of(updatedClient.id(), updatedClient.name(), updatedClient.email(),
-                        updatedClient.phoneNumber(), updatedClient.id()))
-                .update();
-        Assert.state(updated == 1, "Failed to update client " + updatedClient.name());
-    }
+        public void update(Client updatedClient) {
+                var updated = jdbcClient.sql(
+                                "UPDATE Clients SET client_id = ?, client_name = ?, email_address = ?, phone_number = ? WHERE client_id = ?")
+                                .params(List.of(updatedClient.id(), updatedClient.name(), updatedClient.email(),
+                                                updatedClient.phoneNumber(), updatedClient.id()))
+                                .update();
+                Assert.state(updated == 1, "Failed to update client " + updatedClient.name());
+        }
 
-    public List<Client> findAll() {
-        return jdbcClient.sql("SELECT * from clients")
-                .query((rows, rowNum) -> new Client(
-                        rows.getInt("client_id"),
-                        rows.getString("client_name"),
-                        rows.getString("email_address"),
-                        rows.getString("phone_number")))
-                .list();
-    }
+        public List<Client> findAll() {
+                return jdbcClient.sql("SELECT * from clients")
+                                .query((rows, rowNum) -> new Client(
+                                                rows.getInt("client_id"),
+                                                rows.getString("client_name"),
+                                                rows.getString("email_address"),
+                                                rows.getString("phone_number")))
+                                .list();
+        }
 
-    public Client findById(Integer id) {
-        return jdbcClient.sql("SELECT * from Clients WHERE client_id = :id")
-                .param("id", id).query((rows, rowNum) -> new Client(
-                        rows.getInt("client_id"),
-                        rows.getString("client_name"),
-                        rows.getString("email_address"),
-                        rows.getString("phone_number")))
-                .list().get(0);
-    }
+        public Client findById(Integer id) {
+                return jdbcClient.sql("SELECT * from Clients WHERE client_id = :id")
+                                .param("id", id).query((rows, rowNum) -> new Client(
+                                                rows.getInt("client_id"),
+                                                rows.getString("client_name"),
+                                                rows.getString("email_address"),
+                                                rows.getString("phone_number")))
+                                .list().get(0);
+        }
 
-    public Client findByPhoneNumber(String phoneNumber) {
-        return jdbcClient.sql("SELECT * from clients WHERE phone_number = :phone_number")
-                .param("phone_number", phoneNumber).query((rows, rowNum) -> new Client(
-                        rows.getInt("client_id"),
-                        rows.getString("client_name"),
-                        rows.getString("email_address"),
-                        rows.getString("phone_number")))
-                .list().get(0);
-    }
+        public Client findByPhoneNumber(String phoneNumber) {
+                return jdbcClient.sql("SELECT * from clients WHERE phone_number = :phone_number")
+                                .param("phone_number", phoneNumber).query((rows, rowNum) -> new Client(
+                                                rows.getInt("client_id"),
+                                                rows.getString("client_name"),
+                                                rows.getString("email_address"),
+                                                rows.getString("phone_number")))
+                                .list().get(0);
+        }
 
-    public void delete(Integer id) {
-        var updated = jdbcClient.sql("DELETE FROM Clients WHERE client_id = :id")
-                .param("id", id).update();
+        public void delete(Integer id) {
+                var updated = jdbcClient.sql("DELETE FROM Clients WHERE client_id = :id")
+                                .param("id", id).update();
 
-        Assert.state(updated == 1, "Failed to delete client");
-    }
+                Assert.state(updated == 1, "Failed to delete client");
+        }
 }
--- spring-boot/src/main/java/com/mattoi/frambo_mock/product/ProductRepository.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/product/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.mattoi.frambo_mock.product;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,128 +11,156 @@ import org.springframework.util.Assert;
 
 @Repository
 public class ProductRepository {
+        // TODO modify void methods to return booleans depending on success
+        private final JdbcClient jdbcClient;
+        private static final Logger log = LoggerFactory.getLogger(ProductRepository.class);
 
-    private final JdbcClient jdbcClient;
-    private static final Logger log = LoggerFactory.getLogger(ProductRepository.class);
+        public ProductRepository(JdbcClient jdbcClient) {
+                this.jdbcClient = jdbcClient;
+        }
 
-    public ProductRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+        public void create(Product product) {
+                Integer categoryId = jdbcClient
+                                .sql("SELECT category_id FROM Categories WHERE category_name = :category")
+                                .param("category", product.category()).query(Integer.class).single();
+                Assert.state(categoryId != null, "Failed to find a category ID for this name");
+                var updated = jdbcClient
+                                .sql("INSERT INTO Products(product_name, product_description, photo_url, net_weight, price, in_stock, category_id) VALUES (?,?,?,?,?,?,?)")
+                                .params(Arrays.asList(product.name(), product.description(), product.photoUrl(),
+                                                product.netWeight(),
+                                                product.price(), product.inStock(), categoryId))
+                                .update();
+                Assert.state(updated == 1, "Failed to create product " + product.name());
+        }
 
-    public void create(Product product) {
-        Integer categoryId = jdbcClient.sql("SELECT category_id FROM Categories WHERE category_name = :category")
-                .param("category", product.category()).query(Integer.class).single();
-        Assert.state(categoryId != null, "Failed to find a category ID for this name");
-        var updated = jdbcClient
-                .sql("INSERT INTO Products(product_name, product_description, photo_url, net_weight, price, in_stock, category_id) VALUES (?,?,?,?,?,?,?)")
-                .params(List.of(product.name(), product.description(), product.photoUrl(), product.netWeight(),
-                        product.price(), product.inStock(), categoryId))
-                .update();
-        Assert.state(updated == 1, "Failed to create product " + product.name());
-    }
+        public void update(Product product) {
+                Integer categoryId = jdbcClient
+                                .sql("SELECT category_id FROM Categories WHERE category_name = :category")
+                                .param("category", product.category()).query(Integer.class).single();
+                Assert.state(categoryId != null, "Failed to find a category ID for this name");
+                var updated = jdbcClient
+                                .sql("UPDATE Products SET product_id = ?, product_name = ?, product_description = ?, photo_url = ?, net_weight = ?, price = ?, in_stock = ?, category_id = ? WHERE product_id = ?")
+                                .params(Arrays.asList(
+                                                product.id(),
+                                                product.name(),
+                                                product.description(),
+                                                product.photoUrl(),
+                                                product.netWeight(),
+                                                product.price(),
+                                                product.inStock(),
+                                                categoryId,
+                                                product.id()))
+                                .update();
+                Assert.state(updated == 1, "Failed to update product " + product.name());
+        }
 
-    public void update(Product product) {
-        Integer categoryId = jdbcClient.sql("SELECT category_id FROM Categories WHERE category_name = :category")
-                .param("category", product.category()).query(Integer.class).single();
-        Assert.state(categoryId != null, "Failed to find a category ID for this name");
-        var updated = jdbcClient
-                .sql("UPDATE Products SET product_id = ?, product_name = ?, product_description = ?, photo_url = ?, net_weight = ?, price = ?, in_stock = ?, category_id = ? WHERE product_id = ?")
-                .params(List.of(product.id(), product.name(), product.description(), product.photoUrl(),
-                        product.netWeight(), product.price(), product.inStock(), categoryId))
-                .update();
-        Assert.state(updated == 1, "Failed to update product " + product.name());
-    }
+        public void updateProductAvailability(Integer productId, boolean inStock) {
+                var updated = jdbcClient
+                                .sql("UPDATE Products SET in_stock = ? WHERE product_id = ?")
+                                .params(Arrays.asList(inStock, productId))
+                                .update();
+                Assert.state(updated == 1, "Failed to update product");
+        }
 
-    public void updateProductAvailability(Integer productId, boolean inStock) {
-        var updated = jdbcClient
-                .sql("UPDATE Products SET in_stock = ? WHERE product_id = ?").params(List.of(inStock, productId))
-                .update();
-        Assert.state(updated == 1, "Failed to update product");
-    }
+        public List<Product> findAll() {
+                return jdbcClient
+                                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id")
+                                .query((rows, rowNum) -> new Product(
+                                                rows.getInt("product_id"),
+                                                rows.getString("product_name"),
+                                                rows.getString("product_description"),
+                                                rows.getString("photo_url"),
+                                                rows.getInt("net_weight"),
+                                                rows.getFloat("price"),
+                                                rows.getBoolean("in_stock"),
+                                                rows.getString("category_name")))
+                                .list();
+        }
 
-    public List<Product> findAll() {
-        return jdbcClient
-                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id")
-                .query((rows, rowNum) -> new Product(
-                        rows.getInt("product_id"),
-                        rows.getString("product_name"),
-                        rows.getString("product_description"),
-                        rows.getString("photo_url"),
-                        rows.getInt("net_weight"),
-                        rows.getFloat("price"),
-                        rows.getBoolean("in_stock"),
-                        rows.getString("category_name")))
-                .list();
-    }
+        public List<Product> findAllInStock() {
+                return jdbcClient
+                                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id WHERE Products.in_stock = true")
+                                .query((rows, rowNum) -> new Product(
+                                                rows.getInt("product_id"),
+                                                rows.getString("product_name"),
+                                                rows.getString("product_description"),
+                                                rows.getString("photo_url"),
+                                                rows.getInt("net_weight"),
+                                                rows.getFloat("price"),
+                                                rows.getBoolean("in_stock"),
+                                                rows.getString("category_name")))
+                                .list();
+        }
 
-    public List<Product> findAllInStock() {
-        return jdbcClient
-                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id WHERE Products.inStock = 1")
-                .query((rows, rowNum) -> new Product(
-                        rows.getInt("product_id"),
-                        rows.getString("product_name"),
-                        rows.getString("product_description"),
-                        rows.getString("photo_url"),
-                        rows.getInt("net_weight"),
-                        rows.getFloat("price"),
-                        rows.getBoolean("in_stock"),
-                        rows.getString("category_name")))
-                .list();
-    }
+        public Product findByName(String name) {
+                return jdbcClient
+                                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id WHERE Products.product_name = ?")
+                                .param(name)
+                                .query((rows, rowNum) -> new Product(
+                                                rows.getInt("product_id"),
+                                                rows.getString("product_name"),
+                                                rows.getString("product_description"),
+                                                rows.getString("photo_url"),
+                                                rows.getInt("net_weight"),
+                                                rows.getFloat("price"),
+                                                rows.getBoolean("in_stock"),
+                                                rows.getString("category_name")))
+                                .list().get(0);
+        }
 
-    public List<Product> findById(int id) {
-        return jdbcClient
-                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id WHERE Products.product_id = ?")
-                .param(id)
-                .query((rows, rowNum) -> new Product(
-                        rows.getInt("product_id"),
-                        rows.getString("product_name"),
-                        rows.getString("product_description"),
-                        rows.getString("photo_url"),
-                        rows.getInt("net_weight"),
-                        rows.getFloat("price"),
-                        rows.getBoolean("in_stock"),
-                        rows.getString("category_name")))
-                .list();
-    }
+        public Product findById(int id) {
+                return jdbcClient
+                                .sql("SELECT * FROM Products INNER JOIN Categories ON Products.category_id = Categories.category_id WHERE Products.product_id = ?")
+                                .param(id)
+                                .query((rows, rowNum) -> new Product(
+                                                rows.getInt("product_id"),
+                                                rows.getString("product_name"),
+                                                rows.getString("product_description"),
+                                                rows.getString("photo_url"),
+                                                rows.getInt("net_weight"),
+                                                rows.getFloat("price"),
+                                                rows.getBoolean("in_stock"),
+                                                rows.getString("category_name")))
+                                .list().get(0);
+        }
 
-    public void delete(Integer id) {
-        var updated = jdbcClient.sql("DELETE FROM Products WHERE product_id = :id")
-                .param(id).update();
+        public void delete(Integer id) {
+                var updated = jdbcClient.sql("DELETE FROM Products WHERE product_id = ?")
+                                .param(id).update();
 
-        Assert.state(updated == 1, "Failed to delete product");
-    }
+                Assert.state(updated == 1, "Failed to delete product");
+        }
 
-    public void createCategory(Category category) {
-        var updated = jdbcClient
-                .sql("INSERT INTO Categories(category_name) VALUES (?)")
-                .param(category.name())
-                .update();
+        public void createCategory(Category category) {
+                var updated = jdbcClient
+                                .sql("INSERT INTO Categories(category_name) VALUES (?)")
+                                .param(category.name())
+                                .update();
 
-        Assert.state(updated == 1, "Failed to create product " + category.name());
-    }
+                Assert.state(updated == 1, "Failed to create product " + category.name());
+        }
 
-    public void updateCategory(Category category) {
-        var updated = jdbcClient
-                .sql("UPDATE Categories SET category_name = ? WHERE category_id = :id")
-                .param("id", category.id()).update();
+        public void updateCategory(Category category) {
+                var updated = jdbcClient
+                                .sql("UPDATE Categories SET category_name = ? WHERE category_id = ?")
+                                .params(category.name(), category.id()).update();
 
-        Assert.state(updated == 1, "Failed to update product " + category.name());
-    }
+                Assert.state(updated == 1, "Failed to update product " + category.name());
+        }
 
-    public List<Category> findAllCategories() {
-        return jdbcClient
-                .sql("SELECT * FROM Categories")
-                .query((rows, rowNum) -> new Category(
-                        rows.getInt("category_id"),
-                        rows.getString("category_name")))
-                .list();
-    }
+        public List<Category> findAllCategories() {
+                return jdbcClient
+                                .sql("SELECT * FROM Categories")
+                                .query((rows, rowNum) -> new Category(
+                                                rows.getInt("category_id"),
+                                                rows.getString("category_name")))
+                                .list();
+        }
 
-    public void deleteCategory(Integer id) {
-        var updated = jdbcClient.sql("DELETE FROM Categories WHERE category_id = :id")
-                .param(id).update();
+        public void deleteCategory(Integer id) {
+                var updated = jdbcClient.sql("DELETE FROM Categories WHERE category_id = ?")
+                                .param(id).update();
 
-        Assert.state(updated == 1, "Failed to delete category");
-    }
+                Assert.state(updated == 1, "Failed to delete category");
+        }
 }
