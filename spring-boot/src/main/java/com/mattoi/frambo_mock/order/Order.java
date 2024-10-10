a430c7d058377e89ea6d6dd7e0a0a9d242d8cd50
mattoi <matheus.soares.8d@gmail.com>
2024-10-10 18:59:51 -0300

    Added calculation for total amount

--- spring-boot/src/main/java/com/mattoi/frambo_mock/order/Order.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/order/Order.java
@@ -4,13 +4,20 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record Order(
-        Integer id,
-        Integer clientId,
-        Float totalAmount,
-        List<OrderItem> items,
-        String status,
-        LocalDateTime dateCreated,
-        LocalDateTime lastUpdated) {
+                Integer id,
+                Integer clientId,
+                List<OrderItem> items,
+                String status,
+                LocalDateTime dateCreated,
+                LocalDateTime lastUpdated) {
+
+        public Double totalAmount() {
+                Double totalAmount = 0.0;
+                for (OrderItem item : items) {
+                        totalAmount += item.productPrice() * item.quantity();
+                }
+                return totalAmount;
+        }
 }
 
 /*
--- spring-boot/src/main/java/com/mattoi/frambo_mock/order/OrderItem.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/order/OrderItem.java
@@ -1,7 +1,8 @@
 package com.mattoi.frambo_mock.order;
 
 public record OrderItem(
-        Integer productId,
-        String productName,
-        Integer quantity) {
+                Integer productId,
+                String productName,
+                Double productPrice,
+                Integer quantity) {
 }
