59f973e1066fc50fc57e1bc38c4e71683b4e4fe7
mattoi <matheus.soares.8d@gmail.com>
2024-10-09 21:45:58 -0300

    Using status name strings for now

--- spring-boot/src/main/resources/schema.sql
+++ spring-boot/src/main/resources/schema.sql
@@ -3,18 +3,20 @@ CREATE TABLE IF NOT EXISTS Categories (
     category_name VARCHAR(50) UNIQUE NOT NULL,
     PRIMARY KEY (category_id)
 );
+
 CREATE TABLE IF NOT EXISTS Products (
     product_id SERIAL NOT NULL,
     product_name VARCHAR(50) UNIQUE NOT NULL,
     product_description VARCHAR(250) UNIQUE NOT NULL,
     photo_url VARCHAR(250),
     net_weight INT NOT NULL,
-    price DECIMAL(5,2) NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
     in_stock BOOLEAN NOT NULL,
     category_id INT NOT NULL,
     PRIMARY KEY (product_id),
     FOREIGN KEY (category_id) REFERENCES Categories (category_id)
 );
+
 CREATE TABLE IF NOT EXISTS Clients (
     client_id SERIAL NOT NULL,
     client_name VARCHAR(250) NOT NULL,
@@ -22,22 +24,25 @@ CREATE TABLE IF NOT EXISTS Clients (
     phone_number VARCHAR(20) UNIQUE NOT NULL,
     PRIMARY KEY (client_id)
 );
+
 CREATE TABLE IF NOT EXISTS OrderStatus (
     status_id SERIAL NOT NULL,
     status_name VARCHAR(50) UNIQUE NOT NULL,
-    PRIMARY KEY (status_id)
+    PRIMARY KEY (status_name)
 );
+
 CREATE TABLE IF NOT EXISTS Orders (
     order_id SERIAL NOT NULL,
     client_id INT NOT NULL,
-    total_price DECIMAL(10,2) NOT NULL,
-    status_id INT NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    status_name VARCHAR(50) NOT NULL,
     date_created TIMESTAMP NOT NULL,
     last_updated TIMESTAMP NOT NULL,
     PRIMARY KEY (order_id),
     FOREIGN KEY (client_id) REFERENCES Clients (client_id),
-    FOREIGN KEY (status_id) REFERENCES OrderStatus (status_id)
+    FOREIGN KEY (status_name) REFERENCES OrderStatus (status_name)
 );
+
 CREATE TABLE IF NOT EXISTS OrderItems (
     order_id INT NOT NULL,
     product_id INT NOT NULL,
