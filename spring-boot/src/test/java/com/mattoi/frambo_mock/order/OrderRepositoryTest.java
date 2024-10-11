9e6c2931a5901c749ee442b0b6416ec180b5eb6b
mattoi <matheus.soares.8d@gmail.com>
2024-10-11 00:08:38 -0300

    Implemented remaining tests

--- spring-boot/src/test/java/com/mattoi/frambo_mock/order/OrderRepositoryTest.java
+++ spring-boot/src/test/java/com/mattoi/frambo_mock/order/OrderRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -33,6 +34,8 @@ public class OrderRepositoryTest {
     private ProductRepository productRepository;
 
     Integer clientId;
+    List<Product> products = new ArrayList<Product>();
+    List<Order> testOrders = new ArrayList<Order>();
 
     @BeforeEach
     void setup() {
@@ -43,7 +46,12 @@ public class OrderRepositoryTest {
                         "Matheus",
                         "matheus@email.com",
                         "559822222222"));
-        clientId = clientRepository.findByPhoneNumber("559822222222").id();
+        clientRepository.create(
+                new Customer(
+                        null,
+                        "Cecilia",
+                        "cecilia@email.com",
+                        "559811111111"));
         productRepository.createCategory(new Category(null, "Cookie"));
         productRepository.create(new Product(null,
                 "Cookie Original",
@@ -63,59 +71,86 @@ public class OrderRepositoryTest {
                 12.00,
                 true,
                 "Cookie"));
+        clientId = clientRepository.findByPhoneNumber("559822222222").id();
+        products = productRepository.findAll();
+        testOrders = List.of(new Order(null,
+                clientId,
+                List.of(
+                        new OrderItem(
+                                products.get(0).id(),
+                                null,
+                                products.get(0).price(),
+                                2),
+                        new OrderItem(
+                                products.get(1).id(),
+                                null,
+                                products.get(1).price(),
+                                3)),
+                "PAYMENT_PENDING",
+                LocalDateTime.now(),
+                LocalDateTime.now()),
+                new Order(null,
+                        clientId + 1,
+                        List.of(
+                                new OrderItem(
+                                        products.get(0).id(),
+                                        null,
+                                        products.get(0).price(),
+                                        1),
+                                new OrderItem(
+                                        products.get(1).id(),
+                                        null,
+                                        products.get(1).price(),
+                                        1)),
+                        "DELIVERED",
+                        LocalDateTime.now(),
+                        LocalDateTime.now()));
     }
 
     @Test
     public void shouldCreateNewOrder() {
-        // var items = new ArrayList<OrderItem>();
-        var products = productRepository.findAll();
-        var items = List.of(
-                new OrderItem(
-                        products.get(0).id(),
-                        null,
-                        products.get(0).price(),
-                        2),
-                new OrderItem(
-                        products.get(1).id(),
-                        null,
-                        products.get(1).price(),
-                        3));
-        var result = repository.create(new Order(null,
-                clientId,
-                items,
-                "PAYMENT_PENDING",
-                LocalDateTime.now(),
-                LocalDateTime.now()));
+        var result = repository.create(testOrders.get(0));
         assertEquals(true, result);
     }
 
     @Test
     public void shouldUpdateOrderStatus() {
-        // TODO
-        assertEquals(true, false);
+        repository.create(testOrders.get(0));
+        var orderId = repository.findAllByClientId(clientId).get(0).id();
+        var result = repository.updateOrderStatus(orderId, "SHIPPED");
+        assertEquals(true, result);
     }
 
     @Test
     public void shouldFindAllOrders() {
-        assertEquals(true, false);
+        repository.create(testOrders.get(0));
+        repository.create(testOrders.get(1));
+        var orders = repository.findAll();
+        assertEquals(2, orders.size());
     }
 
     @Test
     public void shouldFindOrderById() {
-        // TODO
-        assertEquals(true, false);
+        repository.create(testOrders.get(0));
+        var orderId = repository.findAll().get(0).id();
+        var savedOrder = repository.findById(orderId);
+        assertEquals(orderId, savedOrder.id());
     }
 
     @Test
     public void shouldFindOrdersByClientId() {
-        // TODO
-        assertEquals(true, false);
+        repository.create(testOrders.get(0));
+        repository.create(testOrders.get(1));
+        var ordersFromFirstCustomer = repository.findAllByClientId(testOrders.get(0).customerId());
+        assertEquals(1, ordersFromFirstCustomer.size());
     }
 
     @Test
     public void shouldFindOrdersByStatus() {
-        // TODO
-        assertEquals(true, false);
+        repository.create(testOrders.get(0));
+        repository.create(testOrders.get(1));
+        var pendingOrders = repository.findAllByStatus("PAYMENT_PENDING");
+        assertEquals(1, pendingOrders.size());
     }
 
 }
