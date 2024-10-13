6fbb01af4d859bebeb24f8c34099021748c71b58
mattoi <matheus.soares.8d@gmail.com>
2024-10-13 16:23:43 -0300

    Implemented basic version

--- spring-boot/src/main/java/com/mattoi/frambo_mock/product/ProductController.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/product/ProductController.java
@@ -1,26 +1,75 @@
 package com.mattoi.frambo_mock.product;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-    private ProductRepository productRepository;
+    private ProductRepository repository;
 
     public ProductController(ProductRepository repository) {
-        this.productRepository = repository;
+        this.repository = repository;
     }
 
-    // TODO post create(product fields..)
-    // TODO put update(product fields..)
-    // TODO put setProductAvailability(id, inStock)
-    // TODO get findAll()
-    // TODO get findAllInStock()
-    // TODO get findById(id)
-    // TODO delete delete(id)
-    // TODO post createCategory(name)
-    // TODO put updateCategory(id, newName)
-    // TODO get findAllCategories()
-    // TODO delete deleteCategory(id)
+    @PostMapping("")
+    void create(@ModelAttribute Product product) {
+        repository.create(product);
+    }
+
+    @PutMapping("")
+    void update(@ModelAttribute Product product) {
+        repository.update(product);
+    }
+
+    // todo? put setProductAvailability(id, inStock)
+
+    @GetMapping("")
+    List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    // TODO use a parameter instead of a whole different method
+    @GetMapping("/in_stock")
+    List<Product> findAllInStock() {
+        return repository.findAllInStock();
+    }
+
+    @GetMapping(value = { "" }, params = { "id" })
+    Product findById(@RequestParam(name = "id") Integer id) {
+        return repository.findById(id);
+    }
+
+    @DeleteMapping(value = { "" }, params = { "id" })
+    void delete(@RequestParam(name = "id") Integer id) {
+        repository.delete(id);
+    }
+
+    @PostMapping("/categories")
+    void createCategory(Category category) {
+        repository.createCategory(category);
+    }
+
+    @PutMapping("/categories")
+    void updateCategory(Category category) {
+        repository.updateCategory(category);
+    }
+
+    @GetMapping("/categories")
+    List<Category> findAllCategories() {
+        return repository.findAllCategories();
+    }
+
+    @DeleteMapping(value = { "/categories" }, params = { "id" })
+    void deleteCategory(@RequestParam(name = "id") Integer id) {
+        repository.deleteCategory(id);
+    }
 }
