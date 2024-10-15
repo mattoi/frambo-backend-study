e4d209c1f2767b905b6b851f3eaaa62cfa8f9aeb
mattoi <matheus.soares.8d@gmail.com>
2024-10-14 23:46:47 -0300

    Implemented basic version

--- spring-boot/src/main/java/com/mattoi/frambo_mock/order/OrderController.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/order/OrderController.java
@@ -1,21 +1,53 @@
 package com.mattoi.frambo_mock.order;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
-    private OrderRepository orderRepository;
+    private OrderRepository repository;
 
     public OrderController(OrderRepository repository) {
-        this.orderRepository = repository;
+        this.repository = repository;
     }
 
-    // TODO post create (order fields + order items ..)
-    // TODO put cancelOrder(id)
-    // TODO get findAll()
-    // TODO get findById(id)
-    // TODO get findAllByCustomerId(id)
-    // TODO get findAllByStatus(statusId)
+    @PostMapping("")
+    void create(@RequestBody Order order) {
+        repository.create(order);
+    }
+
+    @PutMapping(value = { "" }, params = { "id" })
+    void cancelOrder(@RequestParam(name = "id") Integer id, @RequestBody HashMap<String, String> status) {
+        repository.updateOrderStatus(id, status.get("status"));
+    }
+
+    @GetMapping("")
+    List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping(value = { "" }, params = { "id" })
+    Order findById(@RequestParam(name = "id") Integer id) {
+        return repository.findById(id);
+    }
+
+    @GetMapping(value = { "" }, params = { "customer_id" })
+    List<Order> findAllByCustomerId(@RequestParam(name = "customer_id") Integer customerId) {
+        return repository.findAllByCustomerId(customerId);
+    }
+
+    @GetMapping(value = { "" }, params = { "status" })
+    List<Order> findAllByStatus(@RequestParam(name = "status") String status) {
+        return repository.findAllByStatus(status);
+    }
 }
