6b678b3c94730b3b75532ba8e5c804044f9446ad
mattoi <matheus.soares.8d@gmail.com>
2024-10-22 17:47:31 -0300

    Placeholder methods

--- spring-boot/src/main/java/com/mattoi/frambo_study/product/ProductService.java
+++ spring-boot/src/main/java/com/mattoi/frambo_study/product/ProductService.java
@@ -1,8 +1,53 @@
 package com.mattoi.frambo_study.product;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.mattoi.frambo_study.exception.EntityNotFoundException;
+import com.mattoi.frambo_study.exception.InvalidRequestException;
 
 @Service
 public class ProductService {
+
+    public int create(Product product) throws InvalidRequestException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    public boolean update(Integer id, Product product) throws InvalidRequestException, EntityNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    public List<Product> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public List<Product> findAllInStock() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllInStock'");
+    }
+
+    public Product findById(Integer id) throws EntityNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    public int createCategory(Category category) throws InvalidRequestException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCategory'");
+    }
+
+    public boolean updateCategory(Integer id, Category category) throws InvalidRequestException, EntityNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCategory'");
+    }
+
+    public List<Category> findAllCategories() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllCategories'");
+    }
 
 }
