de5f904e804a1f12c6457772da096d621ec805d7
mattoi <matheus.soares.8d@gmail.com>
2024-10-22 01:00:04 -0300

    Create returns int, update now only requires modified fields

--- spring-boot/src/main/java/com/mattoi/frambo_mock/customer/CustomerRepository.java
+++ spring-boot/src/main/java/com/mattoi/frambo_mock/customer/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.mattoi.frambo_mock.customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -17,49 +18,68 @@ public class CustomerRepository {
                 this.jdbcClient = jdbcClient;
         }
 
-        public boolean create(Customer customer) {
+        // TODO test this
+        public int create(Customer customer) {
                 var updated = jdbcClient
-                                .sql("INSERT INTO Customers(customer_name, email_address, phone_number) values(?,?,?)")
-                                .params(customer.name(), customer.email(), customer.phoneNumber()).update();
-                return updated == 1;
+                                .sql("INSERT INTO Customers(customer_name, email_address, phone_number) values(?,?,?) RETURNING customer_id")
+                                .params(customer.name(), customer.email(), customer.phoneNumber()).query(int.class).single();
+                return updated;
         }
 
-        public boolean update(Customer updatedClient) {
-                var updated = jdbcClient.sql(
-                                "UPDATE Customers SET customer_id = ?, customer_name = ?, email_address = ?, phone_number = ? WHERE customer_id = ?")
-                                .params(updatedClient.id(), updatedClient.name(), updatedClient.email(),
-                                                updatedClient.phoneNumber(), updatedClient.id())
+        // TODO test this
+        public boolean update(int id, Customer customer){
+                String query =  "UPDATE Customers SET ";
+                var objectFields = new ArrayList<Object>();
+          
+                if (customer.name() != null){
+                        objectFields.add(customer.name());
+                        query += "customer_name = ?, ";
+                }
+                if (customer.email()!= null){
+                        objectFields.add(customer.email());
+                        query += "email_address = ?, ";
+                }
+                if (customer.phoneNumber() != null){
+                        objectFields.add(customer.phoneNumber());
+                        query += "phone_number = ?, ";
+                }
+                objectFields.add(customer.id());
+                // Formatting query to remove last comma and add where
+                query = query.substring(0, query.length() - 2) + "WHERE customer_id = ?";
+
+                var updated = jdbcClient.sql(query)
+                                .params(objectFields)
                                 .update();
                 return updated == 1;
         }
 
         public List<Customer> findAll() {
                 return jdbcClient.sql("SELECT * from Customers ORDER BY customer_id")
-                                .query((rows, rowNum) -> new Customer(
-                                                rows.getInt("customer_id"),
-                                                rows.getString("customer_name"),
-                                                rows.getString("email_address"),
-                                                rows.getString("phone_number")))
+                                .query((rs, rowNum) -> new Customer(
+                                                rs.getInt("customer_id"),
+                                                rs.getString("customer_name"),
+                                                rs.getString("email_address"),
+                                                rs.getString("phone_number")))
                                 .list();
         }
 
         public Customer findById(Integer id) {
                 return jdbcClient.sql("SELECT * from Customers WHERE customer_id = :id")
-                                .param("id", id).query((rows, rowNum) -> new Customer(
-                                                rows.getInt("customer_id"),
-                                                rows.getString("customer_name"),
-                                                rows.getString("email_address"),
-                                                rows.getString("phone_number")))
+                                .param("id", id).query((rs, rowNum) -> new Customer(
+                                                rs.getInt("customer_id"),
+                                                rs.getString("customer_name"),
+                                                rs.getString("email_address"),
+                                                rs.getString("phone_number")))
                                 .list().get(0);
         }
 
         public Customer findByPhoneNumber(String phoneNumber) {
                 return jdbcClient.sql("SELECT * from Customers WHERE phone_number = :phone_number")
-                                .param("phone_number", phoneNumber).query((rows, rowNum) -> new Customer(
-                                                rows.getInt("customer_id"),
-                                                rows.getString("customer_name"),
-                                                rows.getString("email_address"),
-                                                rows.getString("phone_number")))
+                                .param("phone_number", phoneNumber).query((rs, rowNum) -> new Customer(
+                                                rs.getInt("customer_id"),
+                                                rs.getString("customer_name"),
+                                                rs.getString("email_address"),
+                                                rs.getString("phone_number")))
                                 .list().get(0);
         }
 
