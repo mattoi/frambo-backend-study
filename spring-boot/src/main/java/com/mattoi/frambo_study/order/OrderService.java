1cdeeedbd1645795e72362067d5a593e59becb11
mattoi <matheus.soares.8d@gmail.com>
2024-10-23 18:43:24 -0300

    First placeholder

--- spring-boot/src/main/java/com/mattoi/frambo_study/order/OrderService.java
+++ spring-boot/src/main/java/com/mattoi/frambo_study/order/OrderService.java
@@ -2,7 +2,40 @@ package com.mattoi.frambo_study.order;
 
 import org.springframework.stereotype.Service;
 
+import com.mattoi.frambo_study.exception.EntityNotFoundException;
+import com.mattoi.frambo_study.exception.InvalidRequestException;
+
 @Service
 public class OrderService {
+
+    public Object create(Order order) throws InvalidRequestException{
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    public Object updateOrderStatus(Integer id, String string) throws InvalidRequestException, EntityNotFoundException{
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateOrderStatus'");
+    }
+
+    public Object findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public Object findById(Integer id) throws EntityNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    public Object findAllByCustomerId(Integer customerId) throws EntityNotFoundException{
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllByCustomerId'");
+    }
+
+    public Object findAllByStatus(String status) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllByStatus'");
+    }
 
 }
