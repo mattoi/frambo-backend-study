68b7dc326b37be0c1f03134ac8d2d848fbe23806
mattoi <matheus.soares.8d@gmail.com>
2024-10-25 01:25:46 -0300

    First implementation

--- spring-boot/src/main/java/com/mattoi/frambo_study/order/OrderService.java
+++ spring-boot/src/main/java/com/mattoi/frambo_study/order/OrderService.java
@@ -1,41 +1,98 @@
 package com.mattoi.frambo_study.order;
 
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mattoi.frambo_study.exception.CustomerNotFoundException;
 import com.mattoi.frambo_study.exception.EntityNotFoundException;
 import com.mattoi.frambo_study.exception.InvalidRequestException;
+import com.mattoi.frambo_study.exception.ProductsNotFoundException;
 
 @Service
 public class OrderService {
+    @Autowired
+    private OrderRepository repository;
 
-    public Object create(Order order) throws InvalidRequestException{
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    // TODO test these
+    @Transactional
+    public int create(Order order) throws InvalidRequestException {
+        var errors = new ArrayList<String>();
+        if (order.customerId() == null) {
+            errors.add("Customer ID can't be empty");
+        }
+        if (order.totalAmount() <= 0) {
+            errors.add("Total amount has to be higher than zero");
+        }
+
+        for (int i = 0; i > order.items().size() - 1; i++) {
+            if (order.items().get(i).productId() == null) {
+                errors.add("(Item " + i + ": Product ID can't be empty");
+            }
+            if (order.items().get(i).quantity() <= 0) {
+                errors.add("(Item " + i + ": Quantity must be higher than zero");
+            }
+        }
+
+        try {
+            if (errors.size() == 0) {
+                return repository.create(order);
+            } else {
+                throw new InvalidRequestException("Invalid request fields", errors, null);
+            }
+            // TODO find a way to include both customer and products errors; this is only
+            // detecting one or the other
+        } catch (CustomerNotFoundException e) {
+            errors.add(e.getMessage());
+            throw new InvalidRequestException("Invalid request fields", errors, e);
+        } catch (ProductsNotFoundException e) {
+            errors.addAll(e.getMessages());
+            throw new InvalidRequestException("Invalid request fields", errors, e);
+        }
     }
 
-    public Object updateOrderStatus(Integer id, String string) throws InvalidRequestException, EntityNotFoundException{
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateOrderStatus'");
+    @Transactional
+    public boolean updateOrderStatus(Integer id, String newStatus)
+            throws InvalidRequestException, EntityNotFoundException {
+        try {
+            return repository.updateOrderStatus(id, newStatus);
+        } catch (IndexOutOfBoundsException e) {
+            throw new EntityNotFoundException("Couldn't find an order with ID " + id, e);
+        } catch (Exception e) { // TODO find out what exception is this
+            throw new InvalidRequestException("Invalid request field", List.of("Invalid status", e.getMessage()), e);
+            // TODO remove e.getmessages a fter finding out what exception is this
+        }
     }
 
-    public Object findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    @Transactional
+    public List<Order> findAll() {
+        return repository.findAll();
     }
 
-    public Object findById(Integer id) throws EntityNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    @Transactional
+    public Order findById(Integer id) throws EntityNotFoundException {
+        try {
+            return repository.findById(id);
+        } catch (IndexOutOfBoundsException e) {
+            throw new EntityNotFoundException("Couldn't find an order with ID " + id, e);
+        }
     }
 
-    public Object findAllByCustomerId(Integer customerId) throws EntityNotFoundException{
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAllByCustomerId'");
+    @Transactional
+    public List<Order> findAllByCustomerId(Integer customerId) throws EntityNotFoundException {
+        try {
+            return repository.findAllByCustomerId(customerId);
+        } catch (IndexOutOfBoundsException e) {
+            throw new EntityNotFoundException("Couldn't find a customer with ID " + customerId, e);
+        }
     }
 
-    public Object findAllByStatus(String status) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAllByStatus'");
+    @Transactional
+    public List<Order> findAllByStatus(String status) {
+        return repository.findAllByStatus(status);
     }
 
 }
