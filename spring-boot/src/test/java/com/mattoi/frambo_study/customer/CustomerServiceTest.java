a45ad063ab1b022126190923e5366e652063675e
mattoi <matheus.soares.8d@gmail.com>
2024-10-29 17:02:24 -0300

    Fixed and added tests for invalid inputs

--- spring-boot/src/test/java/com/mattoi/frambo_study/customer/CustomerServiceTest.java
+++ spring-boot/src/test/java/com/mattoi/frambo_study/customer/CustomerServiceTest.java
@@ -1,8 +1,8 @@
 package com.mattoi.frambo_study.customer;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,56 +27,105 @@ public class CustomerServiceTest {
 				new Customer(
 						null,
 						"Cecilia",
-						"cecilia@email.com",
-						"559811111111"));
+						null,
+						"5598111111111"));
 		service.create(
 				new Customer(
 						null,
 						"Matheus",
-						"matheus@email.com",
-						"559822222222"));
+						null,
+						"5598222222222"));
 	}
 
 	@Test
-	public void shouldCreateNewCustomer() throws InvalidRequestException {
-		service.create(new Customer(null, "Ayla", "ayla@email.com", "559833333333"));
-		var customers = service.findAll();
-		assertEquals(3, customers.size());
+	public void shouldCreateNewCustomer() {
+		try {
+			service.create(new Customer(null, "Ayla", null, "5598333333333"));
+		} catch (Exception e) {
+			fail("Exception thrown: " + e.getMessage());
+		}
+	}
+
+	@Test
+	public void shouldNotCreateInvalidCustomer() {
+		assertThrows(InvalidRequestException.class, () -> {
+			service.create(new Customer(null, null, null, "559833333333"));
+		});
 	}
 
 	@Test
 	public void shouldFindAllCustomers() {
-		List<Customer> customers = service.findAll();
-		assertEquals(2, customers.size());
+		try {
+			service.findAll();
+		} catch (Exception e) {
+			fail("Exception thrown: " + e.getMessage());
+		}
 	}
 
 	// It's not possible to test findById() with this test framework because
 	// the id of the @BeforeEach customers keeps changing, so I'm using
 	// findByPhoneNumber to get the ID first
 	@Test
-	public void shouldFindByPhoneNumber() throws EntityNotFoundException {
-		var customer = service.findByPhoneNumber("559811111111");
-		assertEquals("Cecilia", customer.name());
+	public void shouldFindByPhoneNumber() {
+		try {
+			var customer = service.findByPhoneNumber("5598111111111");
+			assertEquals("Cecilia", customer.name());
+		} catch (Exception e) {
+			fail("Exception thrown: " + e.getMessage());
+		}
 	}
 
 	@Test
-	public void shouldFindById() throws EntityNotFoundException {
-		var customerFromPhoneNumber = service.findByPhoneNumber("559811111111");
-		var customerFromId = service.findById(customerFromPhoneNumber.id());
-		assertEquals(customerFromId, customerFromPhoneNumber);
+	public void shouldNotFindNonexistentNumber() {
+		assertThrows(EntityNotFoundException.class, () -> {
+			service.findByPhoneNumber("2");
+		});
 	}
 
 	@Test
-	public void shouldUpdateClient() throws EntityNotFoundException, InvalidRequestException {
-		var customer = service.findByPhoneNumber("559811111111");
-		service.update(customer.id(), new Customer(customer.id(), "Matheus Soares", null, null));
-		var updatedClient = service.findByPhoneNumber("559811111111");
-		assertEquals(updatedClient.name(), "Matheus Soares");
+	public void shouldFindById() {
+		try {
+			var customerFromPhoneNumber = service.findByPhoneNumber("5598111111111");
+			var customerFromId = service.findById(customerFromPhoneNumber.id());
+			assertEquals(customerFromId, customerFromPhoneNumber);
+		} catch (Exception e) {
+			fail("Exception thrown: " + e.getMessage());
+		}
 	}
 
+	public void shouldNotFindNonexistentId() {
+		assertThrows(EntityNotFoundException.class, () -> {
+			service.findById(0);
+		});
+	}
+
+	@Test
+	public void shouldUpdateCustomer() {
+		try {
+			var customer = service.findByPhoneNumber("5598111111111");
+			service.update(customer.id(), new Customer(null, "Matheus Soares", null, null));
+			var updatedCustomer = service.findByPhoneNumber("5598111111111");
+			assertEquals(updatedCustomer.name(), "Matheus Soares");
+		} catch (Exception e) {
+			fail("Exception thrown: " + e.getMessage());
+		}
+
+	}
+
+	public void shouldNotUpdateInvalidFields() {
+		assertThrows(InvalidRequestException.class, () -> {
+			service.update(1, new Customer(null, "", null, null));
+		});
+	}
+
+	public void shouldNotUpdateNonexistentId() {
+		assertThrows(EntityNotFoundException.class, () -> {
+			service.update(0, new Customer(null, "Matheus", null, null));
+		});
+	}
 	/*
 	 * @Test
-	 * public void shouldDeleteClient() {
+	 * public void shouldDeleteCustomer() {
 	 * var customer = service.findByPhoneNumber("559811111111");
 	 * service.delete(customer.id());
 	 * var customers = service.findAll();
