3069b81562d6ad6d7881d99a9437a7beb4beb2c1
mattoi <matheus.soares.8d@gmail.com>
2024-10-29 17:01:27 -0300

    Renamed file

--- spring-boot/src/test/java/com/mattoi/frambo_study/TestFramboStudyApplication.java
+++ spring-boot/src/test/java/com/mattoi/frambo_study/TestFramboStudyApplication.java
@@ -2,7 +2,7 @@ package com.mattoi.frambo_study;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestFramboMockApplication {
+public class TestFramboStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.from(FramboStudyApplication::main).with(TestcontainersConfiguration.class).run(args);
