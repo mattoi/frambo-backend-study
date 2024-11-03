e9d0c91ccf17e559a9f4c482550ca5da4a825abe
mattoi <matheus.soares.8d@gmail.com>
2024-11-03 16:19:41 -0300

    Fixed all tests

--- spring-boot/src/test/java/com/mattoi/frambo_study/customer/CustomerServiceTest.java
+++ spring-boot/src/test/java/com/mattoi/frambo_study/customer/CustomerServiceTest.java
@@ -7,16 +7,11 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
 
 import com.mattoi.frambo_study.exception.EntityNotFoundException;
 import com.mattoi.frambo_study.exception.InvalidRequestException;
@@ -29,78 +24,50 @@ public class CustomerServiceTest {
 	@InjectMocks
 	private CustomerService service;
 
-	@BeforeEach
-	void setup() throws InvalidRequestException {
-		service.create(
-				new Customer(
-						null,
-						"Cecilia",
-						null,
-						"5598111111111"));
-		service.create(
-				new Customer(
-						null,
-						"Matheus",
-						null,
-						"5598222222222"));
-	}
+	Customer testCustomer = new Customer(
+			null,
+			"Matheus",
+			null,
+			"5598111111111");
+	Customer invalidCustomer = new Customer(
+			null,
+			null,
+			null,
+			null);
 
-	// * looking fine
 	@Test
 	public void shouldCreateNewCustomer() {
 		try {
-			Customer customer = new Customer(null, "Ayla", null, "5598333333333");
-			/* when(repository.create(customer)).thenReturn(1); */
-			service.create(customer);
+			when(repository.create(testCustomer)).thenReturn(1);
+			assertEquals(1, service.create(testCustomer));
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
 		}
 	}
 
-	// * looking fine
 	@Test
 	public void shouldNotCreateInvalidCustomer() {
 		assertThrows(InvalidRequestException.class, () -> {
-			Customer customer = new Customer(null, null, null, "559833333333");
-			service.create(customer);
+			service.create(invalidCustomer);
 		});
 	}
 
 	@Test
 	public void shouldFindAllCustomers() {
 		try {
-			service.findAll();
+			when(repository.findAll()).thenReturn(List.of(testCustomer));
+			var customers = service.findAll();
+			assertEquals(1, customers.size());
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
 		}
-	}
-
-	// It's not possible to test findById() with this test framework because
-	// the id of the @BeforeEach customers keeps changing, so I'm using
-	// findByPhoneNumber to get the ID first
-	@Test
-	public void shouldFindByPhoneNumber() {
-		try {
-			var customer = service.findByPhoneNumber("5598111111111");
-			assertEquals("Cecilia", customer.name());
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-		}
-	}
-
-	@Test
-	public void shouldNotFindNonexistentNumber() {
-		assertThrows(EntityNotFoundException.class, () -> {
-			service.findByPhoneNumber("2");
-		});
 	}
 
 	@Test
 	public void shouldFindById() {
 		try {
-			var customerFromPhoneNumber = service.findByPhoneNumber("5598111111111");
-			var customerFromId = service.findById(customerFromPhoneNumber.id());
-			assertEquals(customerFromId, customerFromPhoneNumber);
+			when(repository.findById(1)).thenReturn(testCustomer);
+			System.out.println(service.findById(1));
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
 		}
@@ -108,6 +75,8 @@ public class CustomerServiceTest {
 
 	@Test
 	public void shouldNotFindNonexistentId() {
+		when(repository.findById(0))
+				.thenThrow(new EntityNotFoundException("Could not find a user with ID 0", null));
 		assertThrows(EntityNotFoundException.class, () -> {
 			service.findById(0);
 		});
@@ -116,10 +85,9 @@ public class CustomerServiceTest {
 	@Test
 	public void shouldUpdateCustomer() {
 		try {
-			var customer = service.findByPhoneNumber("5598111111111");
-			service.update(customer.id(), new Customer(null, "Matheus Soares", null, null));
-			var updatedCustomer = service.findByPhoneNumber("5598111111111");
-			assertEquals(updatedCustomer.name(), "Matheus Soares");
+			Customer customer = new Customer(null, "Matheus Soares", null, "5598111111111");
+			when(repository.update(1, customer)).thenReturn(true);
+			service.update(1, customer);
 		} catch (Exception e) {
 			fail("Exception thrown: " + e.getMessage());
 		}
@@ -139,13 +107,5 @@ public class CustomerServiceTest {
 			service.update(0, new Customer(null, "Matheus", null, null));
 		});
 	}
-	/*
-	 * @Test
-	 * public void shouldDeleteCustomer() {
-	 * var customer = service.findByPhoneNumber("559811111111");
-	 * service.delete(customer.id());
-	 * var customers = service.findAll();
-	 * assertEquals(1, customers.size());
-	 * }
-	 */
+
 }
