32d9918997b8c869444f02c4f0eb9e102ca9e4e1
mattoi <matheus.soares.8d@gmail.com>
2024-11-03 16:21:11 -0300

    Changed to runtime execptions as they should be

--- spring-boot/src/main/java/com/mattoi/frambo_study/exception/EntityNotFoundException.java
+++ spring-boot/src/main/java/com/mattoi/frambo_study/exception/EntityNotFoundException.java
@@ -1,6 +1,6 @@
 package com.mattoi.frambo_study.exception;
 
-public class EntityNotFoundException extends Exception {
+public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String errorMessage, Throwable err) {
         super(errorMessage, err);
     }
--- spring-boot/src/main/java/com/mattoi/frambo_study/exception/InvalidRequestException.java
+++ spring-boot/src/main/java/com/mattoi/frambo_study/exception/InvalidRequestException.java
@@ -2,14 +2,15 @@ package com.mattoi.frambo_study.exception;
 
 import java.util.List;
 
-public class InvalidRequestException extends Exception {
-private final List<String> errorMessages;
-    public InvalidRequestException(String errorMessage, List<String> errorMessages, Throwable err){
+public class InvalidRequestException extends RuntimeException {
+    private final List<String> errorMessages;
+
+    public InvalidRequestException(String errorMessage, List<String> errorMessages, Throwable err) {
         super(errorMessage);
         this.errorMessages = errorMessages;
     }
 
-    public List<String> getMessages(){
+    public List<String> getMessages() {
         return errorMessages;
     }
 }
